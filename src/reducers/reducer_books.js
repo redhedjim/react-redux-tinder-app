b422504db3f5aa760576c858f815af01eb5839c1
redhedjim <jamesleotucker@gmail.com>
2016-11-27 14:24:32 -0700

    made reducer_books file

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'Javascript: The Good Parts' },
+    { title: 'Harry Potter and the Deathly Hallows' },
+    { title: 'Eloquent Javascript' },
+    { title: 'The Girl with the Dragon Tattoo' },
+  ]
+}
